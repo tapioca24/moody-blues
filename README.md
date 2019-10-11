b4c7a0f853f9d47bb3845d1576f83bf569c02a61
Mitani <h.mitani@elmo.co.jp>
2019-10-11 14:27:37 +0900

    イベントなどを実装

---
+++ README.md
@@ -0,0 +1,107 @@
+# moody-blues
+
+moody-blues is a JavaScript library for HLS players powered by [hls.js](https://github.com/video-dev/hls.js).  
+It has the following features.
+
+- Playlist support
+- Native playback support
+- Easy media operation
+
+## Installation
+
+```sh
+npm install moody-blues
+```
+
+## Usage
+
+```html
+<video id="hls-player" />
+```
+
+```js
+const video = document.getElementById('hls-player')
+const player = new MoodyBlues(video, {
+  debug: true,
+  clip: {
+    source: 'https://example.com/playlist.m3u8'
+  }
+})
+```
+
+## Media operations
+
+There are the following methods for operating media.
+
+| Operation         | Description                                                                                                       |
+| ----------------- | ----------------------------------------------------------------------------------------------------------------- |
+| `play(clips)`     | Play one or more clips.<br>If there is more than one, specify an array of clip objects.                           |
+| `playNext()`      | Plays the next clip in the playlist.                                                                              |
+| `addClips(clips)` | Add one or more clips to the end of the playlist.<br>If there is more than one, specify an array of clip objects. |
+| `resume()`        | Resume playback.                                                                                                  |
+| `pause()`         | Pauses playback.                                                                                                  |
+| `seek(time)`      | Seek to the specified number of seconds.                                                                          |
+| `volume(level)`   | Changes the volume to the specified value.<br>level is a value between 0 and 1.                                   |
+| `mute(muted)`     | Change the muted state.<br>muted is a boolean value.                                                              |
+| `speed(rate)`     | Change the playback speed.                                                                                        |
+
+### Clip object
+
+The clip object has the following properties:
+
+| Property | Type   | Required | Description                                      |
+| -------- | ------ | -------- | ------------------------------------------------ |
+| `source`   | String | Required | Specify the HLS source URL.                      |
+| `start`    | Number | Optional | Specify the number of seconds to start playback. |
+
+## Options
+
+Configration parameters could be provided upon instantiation of `MoodyBlues` object.
+
+| Option                      | Type                                                                                        | Default     | Description                             |
+| --------------------------- | ------------------------------------------------------------------------------------------- | ----------- | --------------------------------------- |
+| `debug`                     | `boolean`                                                                                     | `false`     | Outputs MoodyBlues logs to the console. |
+| `useNativeWheneverPossible` | `boolean`                                                                                     | `false`     | Use native HLS playback if possible.    |
+| `clip`                      | Clip object                                                                                 | `undefined` | Play the specified clip when ready.     |
+| `hlsConfig`                 | [hlsConfig object](https://github.com/video-dev/hls.js/blob/master/docs/API.md#fine-tuning) | `undefined` | hls.js configration                     |
+
+## Events
+
+MoodyBlues fires a bunch of events, that could be registered and unregistered as below:
+
+```js
+const onProgress = (time) => {
+  console.log(time)
+}
+
+// subscribe event
+player.on(MoodyBlues.Events.Progress, onProgress)
+// unsubscribe event
+player.off(MoodyBlues.Events.Progress, onProgress)
+// subscribe for a single event call only
+player.once(MoodyBlues.Events.Progress, onProgress)
+```
+
+| Event                        | Data                                                                                | Description                                                                                               |
+| ---------------------------- | ----------------------------------------------------------------------------------- | --------------------------------------------------------------------------------------------------------- |
+| `MoodyBlues.Events.Finish`   |                                                                                     | Sent when playback completes.                                                                             |
+| `MoodyBlues.Events.Ready`    |                                                                                     | The media's metadata has finished loading.                                                                |
+| `MoodyBlues.Events.Pause`    |                                                                                     | Sent when the playback state is changed to paused.                                                        |
+| `MoodyBlues.Events.Resume`   |                                                                                     | Sent when the playback state is no longer paused.                                                         |
+| `MoodyBlues.Events.Buffer`   | `ranges`: [TimeRanges](https://developer.mozilla.org/en-US/docs/Web/API/TimeRanges) | Sent periodically to inform interested parties of progress downloading the media.                         |
+| `MoodyBlues.Events.Speed`    | `rate: number`                                                                      | Sent when the playback speed changes.                                                                     |
+| `MoodyBlues.Events.Seek`     | `time: number`                                                                      | Sent when a seek operation completes.                                                                     |
+| `MoodyBlues.Events.Progress` | `time: number`                                                                      | The time indicated by the element's currentTime attribute has changed.                                    |
+| `MoodyBlues.Events.Volume`   | `{ volume: number, muted: boolean }`                                                | Sent when the audio volume changes (both when the volume is set and when the muted attribute is changed). |
+| `MoodyBlues.Events.Error`    | `{ type: MoodyBlues.EventTypes, details: string | undefined }`                      | Sent when an error occurs.                                                                                |
+
+### Errors
+
+Each error is categorized by:
+
+- type:
+  - `MoodyBlues.ErrorTypes.NetworkError` for network related errors
+  - `MoodyBlues.ErrorTypes.MediaError` for media/video related errors
+  - `MoodyBlues.ErrorTypes.OtherError` for all other errors
+- details:
+  - errors details
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
+import { EventEmitter } from "eventemitter3";
 import Hls from "hls.js";
-import VideoController from "./VideoController";
 import Playlist from "./Playlist";
 
-class MoodyBlues extends VideoController {
+class MoodyBlues extends EventEmitter {
+  protected video: HTMLVideoElement | null;
   protected config: MoodyBlues.Config;
   protected playlist: Playlist;
   protected hls: Hls | null = null;
 
   constructor(video: HTMLVideoElement, options: MoodyBlues.Options = {}) {
-    super(video);
+    super();
+    this.video = video;
     this.playlist = new Playlist();
+
     this.config = {
       debug: options.debug || false,
       useNativeWheneverPossible: options.useNativeWheneverPossible || false
@@ -20,13 +23,16 @@ class MoodyBlues extends VideoController {
       throw new Error("HLS is not supported");
     }
 
+    // video 要素のイベントハンドラを設定
+    this.setupVideoEventHandlers();
+
     if (this.useNative) {
       this.setupNative(options);
     } else {
       this.setupHls(options);
     }
 
-    this.on(MoodyBlues.Events.Finish, this.playNext.bind(this));
+    this.on(MoodyBlues.Events.Finish, this.playNext, this);
   }
 
   /**
@@ -56,7 +62,7 @@ class MoodyBlues extends VideoController {
   protected setupNative(options: MoodyBlues.Options) {
     this.logger("Use native video element to playback HLS");
 
-    // play initial clip
+    // 最初のクリップを再生する
     if (options.clip) {
       this.play(options.clip);
     }
@@ -67,12 +73,13 @@ class MoodyBlues extends VideoController {
    */
   protected setupHls(options: MoodyBlues.Options) {
     this.logger("Use hls.js to playback HLS");
+
     this.hls = new Hls(options.hlsConfig);
 
-    // setup event handlers
-    this.hls.on(Hls.Events.ERROR, this.onHlsError.bind(this));
+    // hls のイベントハンドラを設定
+    this.setupHlsEventHandlers();
 
-    // play initial clip
+    // 最初のクリップを再生する
     this.hls.attachMedia(this.video!);
     this.hls.once(Hls.Events.MEDIA_ATTACHED, () => {
       if (options.clip) {
@@ -81,11 +88,211 @@ class MoodyBlues extends VideoController {
     });
   }
 
+  /**
+   * hls のイベントハンドラを設定
+   */
+  protected setupHlsEventHandlers() {
+    if (!this.hls) {
+      return;
+    }
+    this.hls.on(Hls.Events.ERROR, this.onHlsError.bind(this));
+  }
+
+  /**
+   * hls のイベントハンドラを削除
+   */
+  protected removeHlsEventHandlers() {
+    if (!this.hls) {
+      return;
+    }
+    this.hls.off(Hls.Events.ERROR, this.onHlsError);
+  }
+
   protected onHlsError(event: "hlsError", data: Hls.errorData) {
-    this.emit(MoodyBlues.Events.Error, {
-      type: data.type,
+    let type: MoodyBlues.ErrorTypes;
+    switch (data.type) {
+      case Hls.ErrorTypes.NETWORK_ERROR:
+        type = MoodyBlues.ErrorTypes.NetworkError;
+        break;
+      case Hls.ErrorTypes.MEDIA_ERROR:
+        type = MoodyBlues.ErrorTypes.MediaError;
+        break;
+      case Hls.ErrorTypes.KEY_SYSTEM_ERROR:
+      case Hls.ErrorTypes.MUX_ERROR:
+      case Hls.ErrorTypes.OTHER_ERROR:
+      default:
+        type = MoodyBlues.ErrorTypes.OtherError;
+    }
+    const errorData: MoodyBlues.ErrorData = {
+      type,
       details: data.details
-    });
+    };
+    this.logger("Error", errorData);
+    this.emit(MoodyBlues.Events.Error, errorData);
+  }
+
+  /**
+   * video 要素のイベンドハンドラを設定
+   */
+  protected setupVideoEventHandlers() {
+    if (!this.video) {
+      return;
+    }
+    this.video.addEventListener("ended", this.onEnded.bind(this));
+    this.video.addEventListener(
+      "loadedmetadata",
+      this.onLoadedmetadata.bind(this)
+    );
+    this.video.addEventListener("pause", this.onPause.bind(this));
+    this.video.addEventListener("play", this.onPlay.bind(this));
+    this.video.addEventListener("progress", this.onProgress.bind(this));
+    this.video.addEventListener("ratechange", this.onRatechange.bind(this));
+    this.video.addEventListener("seeked", this.onSeeked.bind(this));
+    this.video.addEventListener("timeupdate", this.onTimeupdate.bind(this));
+    this.video.addEventListener("volumechange", this.onVolumechange.bind(this));
+    if (this.useNative) {
+      this.video.addEventListener("error", this.onError.bind(this));
+    }
+  }
+
+  /**
+   * video 要素のイベントハンドラを削除
+   */
+  protected removeVideoEventHandlers() {
+    if (!this.video) {
+      return;
+    }
+    this.video.removeEventListener("ended", this.onEnded);
+    this.video.removeEventListener("loadedmetadata", this.onLoadedmetadata);
+    this.video.removeEventListener("pause", this.onPause);
+    this.video.removeEventListener("play", this.onPlay);
+    this.video.removeEventListener("progress", this.onProgress);
+    this.video.removeEventListener("ratechange", this.onRatechange);
+    this.video.removeEventListener("seeked", this.onSeeked);
+    this.video.removeEventListener("timeupdate", this.onTimeupdate);
+    this.video.removeEventListener("volumechange", this.onVolumechange);
+    if (this.useNative) {
+      this.video.removeEventListener("error", this.onError);
+    }
+  }
+
+  protected onEnded(e: Event) {
+    this.logger("Finish");
+    this.emit(MoodyBlues.Events.Finish);
+  }
+
+  protected onLoadedmetadata(e: Event) {
+    this.logger("Ready");
+    this.emit(MoodyBlues.Events.Ready);
+  }
+
+  protected onPause(e: Event) {
+    this.logger("Pause");
+    this.emit(MoodyBlues.Events.Pause);
+  }
+
+  protected onPlay(e: Event) {
+    this.logger("Resume");
+    this.emit(MoodyBlues.Events.Resume);
+  }
+
+  protected onProgress(e: Event) {
+    const video = e.target as HTMLVideoElement
+    this.logger("Buffer", video.buffered);
+    this.emit(MoodyBlues.Events.Buffer, video.buffered);
+  }
+
+  protected onRatechange(e: Event) {
+    const video = e.target as HTMLVideoElement;
+    this.logger("Speed", video.playbackRate);
+    this.emit(MoodyBlues.Events.Speed, video.playbackRate);
+  }
+
+  protected onSeeked(e: Event) {
+    const video = e.target as HTMLVideoElement;
+    this.logger("Seek", video.currentTime);
+    this.emit(MoodyBlues.Events.Seek, video.currentTime);
+  }
+
+  protected onTimeupdate(e: Event) {
+    const video = e.target as HTMLVideoElement;
+    this.logger("Progress", video.currentTime);
+    this.emit(MoodyBlues.Events.Progress, video.currentTime);
+  }
+
+  protected onVolumechange(e: Event) {
+    const video = e.target as HTMLVideoElement;
+    const info = {
+      volume: video.volume,
+      muted: video.muted
+    };
+    this.logger("Volume", info);
+    this.emit(MoodyBlues.Events.Volume, info);
+  }
+
+  protected onError(e: Event) {
+    const video = e.target as HTMLVideoElement;
+    if (video.error) {
+      let type: MoodyBlues.ErrorTypes;
+      switch (video.error.code) {
+        case MediaError.MEDIA_ERR_NETWORK:
+        case MediaError.MEDIA_ERR_SRC_NOT_SUPPORTED:
+          type = MoodyBlues.ErrorTypes.NetworkError;
+          break;
+        case MediaError.MEDIA_ERR_DECODE:
+          type = MoodyBlues.ErrorTypes.MediaError;
+          break;
+        case MediaError.MEDIA_ERR_ABORTED:
+        default:
+          type = MoodyBlues.ErrorTypes.OtherError;
+      }
+      const errorData: MoodyBlues.ErrorData = {
+        type,
+        details: video.error.message
+      };
+      this.logger("Error", errorData);
+      this.emit(MoodyBlues.Events.Error, errorData);
+    }
+  }
+
+  resume() {
+    if (this.video) {
+      this.video.play();
+    }
+  }
+
+  pause() {
+    if (this.video) {
+      this.video.pause();
+    }
+  }
+
+  seek(time: number) {
+    if (!this.video) {
+      return;
+    }
+    if (time < 0 || this.video.duration < time) {
+      return;
+    }
+    this.video.currentTime = time;
+  }
+
+  volume(level: number) {
+    if (this.video) {
+      this.video.volume = level;
+    }
+  }
+
+  mute(muted: boolean) {
+    if (this.video) {
+      this.video.muted = muted
+    }
+  }
+
+  speed(rate: number) {
+    if (this.video) {
+      this.video.playbackRate = rate;
+    }
   }
 
   /**
@@ -129,6 +336,10 @@ class MoodyBlues extends VideoController {
     }
   }
 
+  /**
+   * 既存のプレイリストにクリップを追加する
+   * @param clips 追加するクリップ、またはその配列
+   */
   addClips(clips: MoodyBlues.Clip | MoodyBlues.Clip[]) {
     this.playlist.push(clips);
   }
@@ -138,22 +349,32 @@ class MoodyBlues extends VideoController {
    */
   destroy() {
     this.logger("destroy");
-    super.destroy();
+    if (this.video) {
+      this.removeVideoEventHandlers();
+      this.video = null;
+    }
     this.playlist.clear();
     if (this.hls) {
-      this.hls.off(Hls.Events.ERROR, this.onHlsError);
+      this.removeHlsEventHandlers();
       this.hls.destroy();
     }
     this.removeAllListeners();
   }
 
-  protected logger(message: string) {
+  /**
+   * ログを出力
+   * @param ...args ログ
+   */
+  protected logger(...args: any) {
     if (this.config.debug) {
-      console.log(`[moody-blues] ${message}`);
+      console.log("[moody-blues]", ...args);
     }
   }
 }
 
+/**
+ * MoodyBlues 関連の型定義
+ */
 namespace MoodyBlues {
   export interface Options {
     debug?: boolean;
@@ -185,9 +406,15 @@ namespace MoodyBlues {
     Error = "error"
   }
 
+  export enum ErrorTypes {
+    NetworkError = "networkError",
+    MediaError = "mediaError",
+    OtherError = "otherError"
+  }
+
   export interface ErrorData {
-    type: string;
-    details: string;
+    type: ErrorTypes;
+    details: string | undefined;
   }
 }
 
